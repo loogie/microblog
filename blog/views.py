f774e9a3136d902acc239eaf3aa2b38037616b65
vagrant <vagrant@precise64.(none)>
2013-09-09 18:11:30 +0000

    classify views

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+
+class PublishedPostsMixin(object):
+    
+    def get_queryset(self,):
+        queryset = super(PublishedPostsMixin, self).get_queryset()
+        return queryset.filter(published=True)
+
+class PostListView(PublishedPostsMixin, ListView):
+    model = Post
+    
+    def get_queryset(self,):
+        queryset = super(PostListView, self).get_queryset()
+        return queryset.filter(published=True) 
+
+class PostDetailView(PublishedPostsMixin, DetailView):
+    model = Post
+    
+    def get_queryset(self,):
+        queryset = super(PostDetailView, self).get_queryset()
+        return queryset.filter(published=True)
--- microblog/urls.py
+++ microblog/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+from . import views
+from blog.views import PostListView, PostDetailView
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +11,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^microblog/', include('microblog.foo.urls')),
+    url(r'^$', views.HomepageView.as_view(), name="home"),
+    url(r'^blog/$', PostListView.as_view(), name="blog_list"),
+    url(r'^blog/(?P<pk>\d+)/$', PostDetailView.as_view(), name="blog_detail"),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
